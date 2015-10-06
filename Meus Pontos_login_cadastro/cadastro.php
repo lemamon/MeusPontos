2a9958d06e2fd4d67ff13e51ba6ef2cc9f455773
geojabs <george.bentes@gmail.com>
2015-10-06 13:51:00 -0400

    Comentei algumas linhas de códigos a fim de deixar o arquivo mais didático

--- Meus Pontos_login_cadastro/cadastro.php
+++ Meus Pontos_login_cadastro/cadastro.php
@@ -11,8 +11,10 @@
 	$nome = $_POST["nome"];
 	$email = $_POST["email"];
 	
-	$senha = md5($_POST["senha"]); // Senha criptografada.
+	$senha = md5($_POST["senha"]); // Senha "criptografada".
 	
+	// Continuando com o preechimento de variáveis vindas
+	// formulário.
 	$sexo = $_POST["sexo"];
 	$peso = $_POST["peso"];	
 	$altura = $_POST["altura"];
@@ -21,13 +23,26 @@
 	// Tirando a vírgula e colocando o ponto. (padrão americano)
 	$altura = (float) str_replace(",", ".", $altura);
 
+	// Calculando o IMC
 	$imc  = round($peso/pow($altura, 2), 2);
+	
+	// Aqui estamos pegando a data do sistema para
+	// atribuir como data de nascimento.
 	$dataNasc = date('Y-m-d');
 
+	// Aqui estamos construindo uma instrução SQL para inserção 
+	// no banco de dados.
 	$sql = "INSERT INTO Usuario (us_email, us_senha, us_nome, us_dataNasc, us_sexo, us_altura, us_peso, us_limPontos, us_IMC) VALUES ";
 	$sql .= "('".$email."', '".$senha."', '".$nome."', '".$dataNasc."', '".$sexo."', ".$altura.", ".$peso.", 0, ".$imc.")";
+
+
+	// Aqui estamos executando a instrução SQL criada.
+	// Note que se der erro, a função mysql_error() será chamada 
+	// mostrando qual erro encontrado.
 	mysql_query($sql) or die(mysql_error()." Erro ao inserir");
 
+	// Caso não dê erro, o usuário será redirecionado para
+	// a tela de cadastrado com sucesso.
 	header("Location: cadastrado_sucesso.html");
 
 ?>
