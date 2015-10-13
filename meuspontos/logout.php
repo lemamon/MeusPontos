86c8d17718afbf0252f94df0b3e16a19bb805fe9
lemamon <demmarinho@gmail.com>
2015-10-13 09:47:24 -0400

    Setar o valor do cookie como null, e expirar o tempo de vida do mesmo

--- meuspontos/logout.php
+++ meuspontos/logout.php
@@ -5,6 +5,11 @@
 	// Zeramos as variáveis de sessão
 	$_SESSION["nome_logado"] = "";
 	$_SESSION["email_logado"] = "";
+    
+    if(isset($_COOKIE['lembrar'])) {
+        //Salvando um valor nulo no cookie e expirando o tempo de vida dele.
+        setcookie('lembrar',null,-(time()+(60*60*24*30)));
+    }
 
 	// Chama a função de Destruição da Sessão!
 	session_destroy();
